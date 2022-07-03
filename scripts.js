43e734c1aef057f97a45aceac7b8c1c3da91d378
“serranoankys” <serranoankys@gmail.com>
2022-07-03 16:33:54 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+//var dado = prompt("Qual e sua idade")
+//console.log
+//console.warn(dado)
+console.table(
+{
+  Nome: 'Ankys',
+  Sobrenome: 'Serrano',
+  Idade: '40',
+}
+)
+
+console.table(
+{
+  Nome: 'Ruvi',
+  Sobrenome: 'Piter',
+  Idade : '07',
+
+}
+)
+let pessoa1
+let pessoa2
+let idade1 = prompt('escreva a idade do usuario 1');
+let idade2 = prompt('escreva a idade do usuario 2');
+if('idade1 < idade2'){
+  alert('Olá Ankys a sua idade é ${idade1}');
+  alert ('Olá Ankys a sua idade é:+ idade2');
+  }else if (idade1 > idade2){
+  
+    alert(' O usuario 1 é mais velho que o usuario 2')
+  }else{}
+
+
+
+
+
+
+
+
+
+
+
